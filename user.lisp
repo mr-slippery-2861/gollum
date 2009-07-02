2ff96d5428618a96929518013ece17785e6c0e57
Shelling Chen <fvwmer@gmail.com>
2009-07-02 15:47:30 +0800

    clean up

--- user.lisp
+++ user.lisp
@@ -1,26 +1,20 @@
 (in-package :gollum)
 
-(defmethod current-screen ((obj null))
-  (current-screen (current-display)))
-
-(defmethod current-workspace ((obj null))
-  (current-workspace (current-screen nil)))
-
 (defun raise-or-exec (program &key class name instance)
-  (let ((win (find-matching-window (windows (current-screen nil)) :class class :name name :instance instance)))
+  (let ((win (find-matching-window (windows (current-screen)) :class class :name name :instance instance)))
     (if win
-	(raise-workspace-window win (current-screen nil))
+	(raise-workspace-window win (current-screen))
 	(exec program))))
 
 (defun launch ()
-  (with-screen-input ((current-screen nil) "launch: " program)
+  (with-screen-input ((current-screen) "launch: " program)
     (exec program)))
 
 (defun set-key (keymap key-desc action)
-  (bind-key keymap key-desc action (current-display)))
+  (bind-key keymap key-desc action *display*))
 
 (defun define-keymap (keymap)
-  (add-keymap keymap (current-display)))
+  (add-keymap keymap *display*))
 
 (defun load-rc ()
   (let ((user-rc (probe-file (merge-pathnames (user-homedir-pathname) #p".gollumrc"))))
@@ -28,7 +22,7 @@
 	(load user-rc))))
 
 (defun go-to-workspace (ws)
-  (let* ((screen (current-screen nil))
+  (let* ((screen (current-screen))
 	 (workspace (find-workspace ws (workspaces screen))))
     (when workspace
       (switch-to-workspace workspace screen)
@@ -38,10 +32,10 @@
       (message "workspace ~a does not exist." ws))))
 
 (defun message (control-string &rest format-arguments)
-  (screen-message (current-screen nil) (apply #'format nil control-string format-arguments)))
+  (screen-message (current-screen) (apply #'format nil control-string format-arguments)))
 
 (defun permanent-message (control-string &rest format-arguments)
-  (screen-message (current-screen nil) (apply #'format nil control-string format-arguments) nil))
+  (screen-message (current-screen) (apply #'format nil control-string format-arguments) nil))
 
 (defvar *window-list-resource* :name
   "can be :name :class :instance")
@@ -54,11 +48,11 @@
     (format nil "~a" (funcall resource window))))
 
 (defun window-list (&optional workspace)
-  (let* ((windows (list-windows (or workspace (current-workspace nil))))
+  (let* ((windows (list-windows (or workspace (current-workspace))))
 	 (*internal-window-horizontal-padding* 10)
 	 (window-list (mapcar #'gen-window-list-entry windows)))
     (if window-list
-	(screen-message (current-screen nil) (append (butlast window-list) (list (concat "^|" (car (last window-list))))))
+	(screen-message (current-screen) (append (butlast window-list) (list (concat "^|" (car (last window-list))))))
 	(message "No windows"))))
 
 (defun gen-workspace-list-entry (workspace)
@@ -67,9 +61,9 @@
       (format nil "~a-~a" (id workspace) (name workspace))))
 
 (defun workspace-list (&optional screen)
-  (let ((workspaces (list-workspaces (or screen (current-screen nil))))
+  (let ((workspaces (list-workspaces (or screen (current-screen))))
 	(*internal-window-horizontal-padding* 10))
-    (screen-message (current-screen nil)
+    (screen-message (current-screen)
 		    (mapcar #'gen-workspace-list-entry workspaces))))
 
 (defun date ()
