74b33689f5a8596dc1d98ddb1eb3ae8433d16b6c
Shelling Chen <fvwmer@gmail.com>
2009-08-12 23:37:12 +0800

    better focus processing

---
+++ focus.lisp
@@ -0,0 +1,29 @@
+(in-package :gollum)
+
+;; Input Model 	        Input Field 	WM_TAKE_FOCUS
+;; No Input 	        False 	        Absent             we are not needed to set
+;; Passive 	        True 	        Absent             we are needed to set
+;; Locally Active 	True 	        Present            we are needed to set
+;; Globally Active 	False 	        Present            we are not needed to set
+(defun input-field (window)
+  (let ((hints (xlib:wm-hints (xwindow window))))
+    (if hints
+	(eql (xlib:wm-hints-input hints) :on)
+	nil)))
+
+(defun should-grab-input (window)
+  (if (input-field window)
+      t
+      nil))
+
+(defun set-net-active-window (root id)
+  (xlib:change-property (xmaster root)
+			:_NET_ACTIVE_WINDOW
+			(list id)
+			:window 32))
+
+(defun set-focus (window)
+  (if window
+      (let ((root (root (screen window))))
+	(set-input-focus window :pointer-root)
+	(set-net-active-window root (xlib:window-id (xwindow window))))))
